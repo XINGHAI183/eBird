480ae2bef5521c5bddb23d4b2cd8448116fc32e6
XINGHAI <majiangge2022@outlook.com>
2026-04-29 20:49:20 +0800

    feat(页面加载): 为所有页面添加加载动画

--- pages/architecture/architecture.js
+++ pages/architecture/architecture.js
@@ -1,9 +1,37 @@
 Page({
   data: {
+    isLoading: true,
+    hasLoaded: false
   },
   onLoad() {
+    // 如果是首次加载，显示加载动画
+    if (!this.data.hasLoaded) {
+      setTimeout(() => {
+        this.setData({
+          isLoading: false,
+          hasLoaded: true
+        });
+      }, 800); // 模拟加载过程，实际项目中可以替换成真实的数据加载逻辑
+    } else {
+      this.setData({
+        isLoading: false
+      });
+    }
   },
   onShow() {
+    // 只有在未加载过的情况下，再次进入页面才显示加载动画
+    if (!this.data.hasLoaded) {
+      this.setData({
+        isLoading: true
+      });
+      setTimeout(() => {
+        this.setData({
+          isLoading: false,
+          hasLoaded: true
+        });
+      }, 800);
+    }
+    
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         selected: 2
